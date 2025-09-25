33c275be1372cbda02f6b9d26d6fcd340b4d7de4
Manuel Pichiri <manuk95@live.it>
2025-09-25 17:07:09 +0200

    EsercizioM3W3D1 Aggiornato

--- main.js
+++ main.js
@@ -25,3 +25,43 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+const bookmark = document.querySelector(".bookmark-custom");
+const bookmarks = document.querySelectorAll(".bi-bookmark");
+const cards = document.querySelectorAll(".swiper-slide .bg-light");
+const cardsImg = document.querySelectorAll(".bg-light img");
+
+function addClassCard() {
+  cards.forEach((card) => {
+    card.classList.add("card-element");
+  });
+}
+
+function bookmarkFill(bookmarks) {
+  bookmarks.addEventListener("mouseenter", () => {
+    bookmarks.classList.replace("bi-bookmark", "bi-bookmark-fill");
+  });
+}
+
+function bookmarkEmpty(bookmarks) {
+  bookmarks.addEventListener("mouseleave", () => {
+    bookmarks.classList.replace("bi-bookmark-fill", "bi-bookmark");
+  });
+}
+
+function addClassCardImg() {
+  cardsImg.forEach((cardImg) => {
+    cardImg.classList.add("rounded-2");
+  });
+}
+
+function allBookmark() {
+  bookmarks.forEach((bookmark) => {
+    bookmarkFill(bookmark);
+    bookmarkEmpty(bookmark);
+  });
+}
+
+addClassCard();
+addClassCardImg();
+allBookmark();
